fb42a5b72a3a41d77154473b09afdfac650749a3
shinhojung <shinhojung814@gmail.com>
2025-11-13 00:59:26 +0900

    feat: update rag pipeline functions

--- llm.py
+++ llm.py
@@ -1,7 +1,19 @@
+from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.chat_history import BaseChatMessageHistory
+from langchain_core.runnables.history import RunnableWithMessageHistory
+from langchain_classic.chains import create_history_aware_retriever, create_retrieval_chain
+from langchain_classic.chains.combine_documents import create_stuff_documents_chain
+from langchain_community.chat_message_histories import ChatMessageHistory
 from langchain_openai import ChatOpenAI, OpenAIEmbeddings
 from langchain_pinecone import PineconeVectorStore
-from langchain_core.prompts import ChatPromptTemplate
-from langchain_core.output_parsers import StrOutputParser
+
+store = {}
+
+def get_session_history(session_id: str) -> BaseChatMessageHistory:
+    if session_id not in store:
+        store[session_id] = ChatMessageHistory()
+    return store[session_id]
 
 # LLM 설정
 def get_llm(model='gpt-4o'):
@@ -18,68 +30,101 @@ def get_retriever(index_name='tax-markdown-index'):
 
     return retriever
 
-# 7. 사전 정의 및 질문 변환용 프롬프트 반환
-def get_dictionary_chain():
-    llm = get_llm()
-
-    dictionary = ["사람을 나타내는 표현 -> 거주자"]
-
-    dictionary_prompt = ChatPromptTemplate.from_template("""당신은 한국 소득세 법의 전문가입니다.
-
-    사용자의 질문을 읽고, 아래 사전을 참고해서 질문을 더 정확한 법률 용어로 변경해주세요.
-    만약 변경할 필요가 없다고 판단되면 원래 질문을 그대로 반환해주세요.
-
-    사전: {dictionary}
-
-    기존 질문: {question}
-
-    변경된 질문:""")
-
-    dictionary_chain = (
-        {
-            "dictionary": lambda _: dictionary,
-            "question": lambda x: x["question"]
-        }
-        |dictionary_prompt
-        | llm
-        | StrOutputParser()
-    )
-    return dictionary_chain
-
-# 4. QA 체인 설정 및 답변 생성용 프롬프트 반환
-def get_qa_chain():
+def get_history_aware_retriever():
     llm = get_llm()
     retriever = get_retriever()
 
-    answer_prompt = ChatPromptTemplate.from_template("""[Identity]
-    - 당신은 한국 소득세 법의 전문가입니다.
-    - [Context]를 참고해서 사용자의 질문에 답변해주세요.
-
-    [Context]
-    {context}
-
-    질문: {question}
-
-    Answer:""")
-
-    qa_chain = (
-        {
-            "context": lambda x: "\n\n".join(doc.page_content for doc in retriever.invoke(x["query"])),
-            "question": lambda x: x["query"]
-        }
-        | answer_prompt
-        | llm
-        | StrOutputParser()
+    contextualize_q_system_prompt = (
+        "Given a chat history and the latest user question "
+        "which might reference context in the chat history, "
+        "formulate a standalone question which can be understood "
+        "without the chat history. Do NOT answer the question, "
+        "just reformulate it if needed and otherwise return it as is."
     )
 
-    return qa_chain
+    contextualize_q_prompt = ChatPromptTemplate.from_messages(
+        [
+            ("system", contextualize_q_system_prompt),
+            MessagesPlaceholder("chat_history"),
+            ("human", "{input}")
+        ]
+    )
+
+    history_aware_retriever = create_history_aware_retriever(
+        llm, retriever, contextualize_q_prompt
+    )
+
+    return history_aware_retriever
+
+# 사전 정의 및 질문 변환용 프롬프트 반환
+def get_dictionary_chain():
+    llm = get_llm()
+    dictionary = ["사람을 나타내는 표현 -> 거주자"]
+
+    prompt = ChatPromptTemplate.from_template(f"""
+        사용자의 질문을 읽고, 아래 사전을 참고해서 질문을 더 정확한 법률 용어로 변경해주세요.
+        만약 변경할 필요가 없다고 판단되면 입력된 질문을 그대로 반환해주세요.
+
+        사전: {dictionary}
+
+        질문: {{question}}
+    """)
+
+    dictionary_chain = prompt | llm | StrOutputParser()
+    
+    return dictionary_chain
+
+# QA 체인 설정 및 답변 생성용 프롬프트 반환
+def get_rag_chain():
+    llm = get_llm()
+    history_aware_retriever = get_history_aware_retriever()
+
+    system_prompt = (
+        "You are an assistant for question-answering tasks. "
+        "Use the follow pieces of retrieved context to answer "
+        "the question. If you don't know the answer, say that you "
+        "don't know. Use three sentences maximum and keep the "
+        "answer concise."
+        "\n\n"
+        "{context}"
+    )
+
+    qa_prompt = ChatPromptTemplate.from_messages(
+        [
+            ("system", system_prompt),
+            MessagesPlaceholder("chat_history"),
+            ("human", "{input}")
+        ]
+    )
+
+    question_answer_chain = create_stuff_documents_chain(llm, qa_prompt)
+
+    rag_chain = create_retrieval_chain(history_aware_retriever, question_answer_chain)
+
+    conversational_rag_chain = RunnableWithMessageHistory(
+        rag_chain,
+        get_session_history,
+        input_messages_key="input",
+        history_messages_key="chat_history",
+        output_messages_key="answer"
+    ).pick("answer")
+    
+    return conversational_rag_chain
 
 # 소득세 챗봇 체인 설정 및 답변 반환
-def get_ai_message(user_message):
+def get_ai_response(user_message):
     dictionary_chain = get_dictionary_chain()
-    qa_chain = get_qa_chain()
+    rag_chain = get_rag_chain()
+    tax_chain = {"input": dictionary_chain} | rag_chain
+    ai_response = tax_chain.stream(
+        {
+            "question": user_message
+        },
+        config={
+            "configurable": {
+                "session_id": "abc123"
+            }
+        }
+    )
 
-    tax_chain = {"query": dictionary_chain} | qa_chain
-    ai_message = tax_chain.invoke({"question": user_message})
-
-    return ai_message
+    return ai_response
